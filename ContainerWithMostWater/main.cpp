06b4c3eb343e05b457977e6d28529addbd24ce1c
sc.lin <read0651@hotmail.com>
2022-07-11 00:16:35 +0800

    New: Created project about container with most water.

---
+++ ContainerWithMostWater/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include "AssertClass.hpp"
+#include "VectorMiscs.hpp"
+
+class ContainerWithMostWater
+{
+public:
+	ContainerWithMostWater()
+	{}
+
+	~ContainerWithMostWater()
+	{}
+
+	int Solution(std::vector<int> &height)
+	{
+	}
+
+private:
+};
+
+class TestContainerWithMostWater
+{
+public:
+	TestContainerWithMostWater()
+	{}
+
+	~TestContainerWithMostWater()
+	{}
+
+
+private:
+
+};
+
+int main(int argc, char** argv)
+{
+	getchar();
+	return EXIT_SUCCESS;
+}
