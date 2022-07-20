77b15d5c0156e68fb6cd182a500d70f773a7bd01
Bill <read0651@hotmail.com>
2022-07-20 13:47:25 +0800

    New: Created removed n-th from end of list project.

---
+++ 0019RemoveNthFromeEndOfList/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include "AssertClass.hpp"
+#include "VectorMiscs.hpp"
+#include "LeetcodeLinkListMiscs.hpp"
+
+
+class Solution
+{
+public:
+	Solution()
+	{}
+
+	~Solution()
+	{}
+
+	ListNode* RemoveNthFromEnd(ListNode* head, int n)
+	{
+	}
+
+private:
+};
+
+class TestRemoveNthFromEnd
+{
+public:
+	TestRemoveNthFromEnd()
+	{}
+
+	~TestRemoveNthFromEnd()
+	{}
+
+
+private:
+	Solution mSolution;
+
+};
+
+int main(int argc, char** argv)
+{
+	TestRemoveNthFromEnd test;
+	getchar();
+	return EXIT_SUCCESS;
+}
