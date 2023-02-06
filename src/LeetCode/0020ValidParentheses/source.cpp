fcd2bc7f1142d6399cb9c969516f89db7007a6de
sclin <read0651@hotmail.com>
2023-02-06 16:54:12 +0800

    New: Created valid parentheses project and finished it.

---
+++ src/LeetCode/0020ValidParentheses/source.cpp
@@ -0,0 +1,128 @@
+#include <iostream>
+#include <string>
+#include <stack>
+#include "AssertClass.hpp"
+
+class Solution
+{
+public:
+	Solution()
+	{}
+
+	~Solution()
+	{}
+	
+	bool isValid(std::string s) {
+		bool valid = true;
+		std::stack<char> parentheses;
+		for (auto c : s) {
+			if (c == '(' || c == '[' || c == '{') {
+				parentheses.push(c);
+			}
+			else {
+				if (c == ')') {
+					if (parentheses.empty() || parentheses.top() != '(') {
+						valid = false;
+						break;
+					}
+					parentheses.pop();
+				}
+				else if (c == ']') {
+					if (parentheses.empty() || parentheses.top() != '[') {
+						valid = false;
+						break;
+					}
+					parentheses.pop();
+				}
+				else if (c == '}') {
+					if (parentheses.empty() || parentheses.top() != '{') {
+						valid = false;
+						break;
+					}
+					parentheses.pop();
+				}
+			}
+		}
+
+		if (!parentheses.empty()) {
+			valid = false;
+		}
+
+		return valid;
+    }
+};
+
+class TestThreeSum
+{
+public:
+	TestThreeSum()
+	{}
+
+	~TestThreeSum()
+	{}
+
+	void Example1()
+	{
+		std::string input = "()";
+		std::cout << "===== Test input \"" << input << "\", output true =====" << std::endl;
+
+		bool reslut = this->mSolution.isValid(input);
+
+		AssertClass::GetInstance().Assert(reslut == true);
+	}
+
+	void Example2()
+	{
+		std::string input = "()[]{}";
+		std::cout << "===== Test input \"" << input << "\", output true =====" << std::endl;
+
+		bool reslut = this->mSolution.isValid(input);
+
+		AssertClass::GetInstance().Assert(reslut == true);
+	}
+
+	void Example3()
+	{
+		std::string input = "(]";
+		std::cout << "===== Test input \"" << input << "\", output false =====" << std::endl;
+
+		bool reslut = this->mSolution.isValid(input);
+
+		AssertClass::GetInstance().Assert(reslut == false);
+	}
+
+	void Example4()
+	{
+		std::string input = "]";
+		std::cout << "===== Test input \"" << input << "\", output false =====" << std::endl;
+
+		bool reslut = this->mSolution.isValid(input);
+
+		AssertClass::GetInstance().Assert(reslut == false);
+	}
+
+	void Example5()
+	{
+		std::string input = "(())";
+		std::cout << "===== Test input \"" << input << "\", output true =====" << std::endl;
+
+		bool reslut = this->mSolution.isValid(input);
+
+		AssertClass::GetInstance().Assert(reslut == true);
+	}
+
+private:
+	Solution mSolution;
+};
+
+int main(int argc, char** argv)
+{
+	TestThreeSum test;
+	test.Example1();
+	test.Example2();
+	test.Example3();
+	test.Example4();
+	test.Example5();
+	getchar();
+	return EXIT_SUCCESS;
+}
