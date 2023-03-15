66435c20095e6a2a3742a0a69ec092ceb87304f7
sclin <read0651@hotmail.com>
2023-03-15 16:52:16 +0800

    Chg: Defined LTestTuple class field. Chg: Finished leetcode test framework unit test project setting.

--- src/LeetcodeTestFramework.UnitTest/main.cpp
+++ src/LeetcodeTestFramework.UnitTest/main.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <gtest/gtest.h>
+#include "LTestTuple.hpp"
+
+TEST(HelloWorldTest, TestLTestTuple)
+{
+	LTestTuple tuple(123);
+
+	EXPECT_EQ(tuple.GetType(), LTestTupleType::IntType);
+	EXPECT_EQ(tuple.GetIntValue(), 123);
+}
 
 int main(int argc, char** argv)
 {
-	return EXIT_SUCCESS;
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
 }
--- src/LeetcodeTestFramework/LTestTuple.hpp
+++ src/LeetcodeTestFramework/LTestTuple.hpp
@@ -1,8 +1,76 @@
 #pragma once
 
+#include <iostream>
+#include <vector>
+#include <string>
+#include "LeetCodeGraphMiscs.hpp"
+#include "LeetCodeLinkListMiscs.hpp"
+#include "LeetCodeTreeMiscs.hpp"
+
+enum class LTestTupleType : int
+{
+	None,
+	IntType,
+	DoubleType,
+	StrType,
+	Int1DArrType,
+	Int2DArrType,
+	Double1DArrType,
+	Double2DArrType,
+	Str1DArrType,
+	Str2DArrType,
+	LinkListType,
+	TreeType,
+	GraphType,
+};
+
 class LTestTuple
 {
 public:
+	// Copy Constructor
+	LTestTuple(LTestTuple &tup)
+	{}
+
+	// Move Constructor
+	LTestTuple(LTestTuple &&tup)
+	{}
+
+	LTestTuple(int val)
+	{
+		this->mType = LTestTupleType::IntType;
+		this->mIValue = val;
+	}
+
+	LTestTupleType GetType()
+	{
+		return this->mType;
+	}
+
+	int GetIntValue()
+	{
+		if (this->mType != LTestTupleType::IntType)
+			throw "This LTestTuple not IntType";
+
+		return this->mIValue;
+	}
+
+	std::vector<int>& GetInt1DArray()
+	{
+		return this->mI1DArrValue;
+	}
 
 private:
+	LTestTupleType mType = LTestTupleType::None;	
+	int mIValue = 0.0;
+	double mDValue = 0.0;
+	std::string mSValue = "";
+	std::vector<int> mI1DArrValue;
+	std::vector<std::vector<int>> mI2DArrValue;
+	std::vector<double> mD1DArrValue;
+	std::vector<std::vector<double>> mD2DArrValue;
+	std::vector<std::string> mS1DArrValue;
+	std::vector<std::vector<std::string>> mS2DArrValue;
+	ListNode* mListHeadValue = nullptr;
+	TreeNode* mTreeRootValue = nullptr;
+	Node* mGraphValue = nullptr;
 };
