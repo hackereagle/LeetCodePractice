f4aacbad075b1608ffeeee2f9291e0abdd47ee97
sclin <read0651@hotmail.com>
2023-09-10 14:18:40 +0800

    Finished k-th smallest element in a sorted matrix project.

--- src/LeetCode/0378KthSmallestElementInASortedMatrix/source.cpp
+++ src/LeetCode/0378KthSmallestElementInASortedMatrix/source.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <string>
+#include <vector>
+#include <queue>
+#include "VectorMiscs.hpp"
 #include "MyPriorityQueueTest.hpp"
 #include "AssertClass.hpp"
+
+#define USING_SELF_DEF_PQ 1
 
 class Solution
 {
@@ -12,29 +17,74 @@ public:
 	~Solution()
 	{}
 	
-	bool isValid(std::string s) {
-		return false;
+	int kthSmallest(std::vector<std::vector<int>>& matrix, int k) {
+	#ifdef USING_SELF_DEF_PQ
+		MyPriorityQueue pq = MyPriorityQueue(std::greater<int>());
+	#else
+        std::priority_queue<int, std::vector<int>, std::greater<int>> pq;
+	#endif
+        for (auto r : matrix) {
+            for (auto c : r) {
+	#ifdef USING_SELF_DEF_PQ
+				pq.Push(c);
+	#else
+                pq.push(c);
+	#endif
+            }
+        }
+
+        int ret = 0;
+        while(k >= 1) {
+	#ifdef USING_SELF_DEF_PQ
+			ret = pq.Top();
+			pq.Pop();
+	#else
+            ret = pq.top();
+            pq.pop();
+	#endif
+            //std::cout << ret << " "; // debug
+            k = k - 1;
+        }
+        //std::cout << std::endl; // debug
+
+        return ret;
     }
 };
 
-class TestIsValid
+class TestKthSmallest
 {
 public:
-	TestIsValid()
+	TestKthSmallest()
 	{}
 
-	~TestIsValid()
+	~TestKthSmallest()
 	{}
 
 	void Example1()
 	{
 		// ARRANGE
-		std::string input = "()";
-		bool expectedOutput = true;
-		std::cout << "===== Test input " << input << "; output true =====" << std::endl;
+		std::vector<std::vector<int>> matrix({{1,5,9},{10,11,13},{12,13,15}});
+		int k = 8;
+		int expectedOutput = 13;
+		std::cout << "===== Test matrix = " << Vector2Str(matrix) << ", k = " << k << "; output " << expectedOutput << " =====" << std::endl;
 
 		// ACT
-		bool result = this->mSolution.isValid(input);
+		int result = this->mSolution.kthSmallest(matrix, k);
+
+		// ASSERT
+		AssertClass::GetInstance().Assert(result == expectedOutput);
+	}
+	
+	void Example2()
+	{
+		// ARRANGE
+		std::vector<std::vector<int>> matrix({{-5}});
+		int k = 1;
+		int expectedOutput = -5;
+		std::cout << "===== Test matrix = " << Vector2Str(matrix) << ", k = " << k << "; output " << expectedOutput << " =====" << std::endl;
+
+		// ACT
+		int result = this->mSolution.kthSmallest(matrix, k);
 
 		// ASSERT
 		AssertClass::GetInstance().Assert(result == expectedOutput);
@@ -46,15 +96,16 @@ private:
 
 int main(int argc, char** argv)
 {
-	MyPriorityQueueTest myPriorityQueueTest;	
-	myPriorityQueueTest.TestSameResultWithCppStlPriorityQueue_Default();
-	myPriorityQueueTest.TestSameResultWithCppStlPriorityQueue_Greater();
-	myPriorityQueueTest.TestPriorityQueueEmpty();
-	myPriorityQueueTest.TestPriorityQueueEmptyAfterPop();
-	myPriorityQueueTest.TestPriorityQueueAfterEmpty();
+	// MyPriorityQueueTest myPriorityQueueTest;	
+	// myPriorityQueueTest.TestSameResultWithCppStlPriorityQueue_Default();
+	// myPriorityQueueTest.TestSameResultWithCppStlPriorityQueue_Greater();
+	// myPriorityQueueTest.TestPriorityQueueEmpty();
+	// myPriorityQueueTest.TestPriorityQueueEmptyAfterPop();
+	// myPriorityQueueTest.TestPriorityQueueAfterEmpty();
 
-	// TestIsValid test;
-	// test.Example1();
+	TestKthSmallest test;
+	test.Example1();
+	test.Example2();
 	getchar();
 	return EXIT_SUCCESS;
 }
