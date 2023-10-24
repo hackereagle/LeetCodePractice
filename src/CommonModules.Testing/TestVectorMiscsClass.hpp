20063471d99cdeaeb7fed95993e7cf83a8925f8e
sclin <read0651@hotmail.com>
2023-10-24 21:11:18 +0800

    Added std::vector<std::vector> testing cases.

--- src/CommonModules.Testing/TestVectorMiscsClass.hpp
+++ src/CommonModules.Testing/TestVectorMiscsClass.hpp
@@ -71,83 +71,28 @@ TEST(TestVectorMiscsClass, TestCheckingTwoVectorNotSimilar)
 	EXPECT_FALSE(isSimilar);
 }
 
-// class TestVectorMiscsClass
-// {
-// public:
-// 	TestVectorMiscsClass()
-// 	{}
+TEST(TestVectorMiscsClass, TestVector2Str_VectorVectorInt)
+{
+	// ARRANGE
+	std::vector<std::vector<int>> data;
+	data.push_back({1, 2, 3});
+	data.push_back({4, 5});
 
-// 	~TestVectorMiscsClass()
-// 	{}
+	// ACT
+	std::string str = Vector2Str(data);
 
-// 	void TestPrintAcceptTypeVector()
-// 	{
-// 		std::cout << "========== Test Print Accept Type Vector ==========" << std::endl;
-// 		std::cout << "" << std::endl;
-// 		std::vector<int> test(3, 500);
-// 		PrintVector<int>(test);
-// 	}
+	// ASSERT
+	EXPECT_EQ(str, "[[1, 2, 3], [4, 5]]");
+}
 
-// 	void TestPrintNotAcceptTypeVector()
-// 	{
-// 		std::cout << "========== Test Print Not Accept Type Vector ==========" << std::endl;
-// 		std::vector<struct otherStruct> test;
-// 		test.push_back(OtherStruct(1));
-// 		test.push_back(OtherStruct(1));
-// 		PrintVector<struct otherStruct>(test);
-// 	}
+TEST(TestVectorMiscsClass, TestVector2Str_VectorVectorChar)
+{
+	// ARRANGE
+	std::vector<std::vector<char>> data({{'a', 'b', 'c'}, {'d', 'e', 'f'}});
 
-// 	void TestCheckingTwoVectorSimilar()
-// 	{
-// 		std::cout << "========== Test Checking Two Vector Similar ==========" << std::endl;
-// 		std::vector<int> v1;
-// 		v1.push_back(1);
-// 		v1.push_back(2);
-// 		v1.push_back(3);
-// 		PrintVector<int>(v1);
+	// ACT
+	std::string str = Vector2Str(data);
 
-// 		std::vector<int> v2;
-// 		v2.push_back(2);
-// 		v2.push_back(1);
-// 		v2.push_back(3);
-// 		PrintVector<int>(v2);
-
-// 		bool isSimilar = IsTwoVectorSimilar<int>(v1, v2);
-// 		AssertClass::GetInstance().Assert(isSimilar == true);
-// 	}
-
-// 	void TestCheckingTwoVectorNotSimilar()
-// 	{
-// 		std::cout << "========== Test Checking Two Vector Not Similar ==========" << std::endl;
-// 		std::vector<int> v1;
-// 		v1.push_back(1);
-// 		v1.push_back(2);
-// 		v1.push_back(3);
-// 		PrintVector<int>(v1);
-
-// 		std::vector<int> v2;
-// 		v2.push_back(2);
-// 		v2.push_back(1);
-// 		v2.push_back(5);
-// 		PrintVector<int>(v2);
-
-// 		bool isSimilar = IsTwoVectorSimilar<int>(v1, v2);
-// 		AssertClass::GetInstance().Assert(isSimilar == false);
-// 	}
-
-// private:
-// 	typedef struct otherStruct{
-// 		otherStruct(int val)
-// 		{
-// 			member = val;
-// 		}
-// 		int member = 0;
-
-// 		friend std::ostream& operator<<(std::ostream& stream, const otherStruct& s)
-// 		{
-// 			stream << s.member;
-// 			return stream;
-// 		}
-
-// 	}OtherStruct;
-// };
+	// ASSERT
+	EXPECT_EQ(str, "[[\"a\", \"b\", \"c\"], [\"d\", \"e\", \"f\"]]");
+}
